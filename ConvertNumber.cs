7d9a6c471f83f7c7d4cc2f3728c9bf113b29a44d
Mahdi Hashemi <Mahdi008397@gmail.com>
2022-01-18 12:43:39 +0330

    first Commit

---
+++ ConvertNumber.cs
@@ -0,0 +1,22 @@
+﻿namespace ConvertNumber;
+
+
+public static class ConvertNumber
+{
+    public static string ConvertToK(this int count)
+    {
+        decimal res;
+
+        if (count <= 1000) return count.ToString();
+        if (count >= 1000000)
+        {
+            res = decimal.Divide(count, 1000000);
+            return $"{res}M";
+        }
+        else
+        {
+            res = decimal.Divide(count, 1000);
+            return $"{res}K";
+        }
+    }
+}
